f6e1423bc04724e770157c833bdeb5814bfd5b4f
Franz Banua <franzbanua@gmail.com>
2022-12-06 14:31:57 +0800

    Update portfolio_covid_data_exploration.sql

--- portfolio_covid_data_exploration.sql
+++ portfolio_covid_data_exploration.sql
@@ -43,17 +43,12 @@ AND continent IS NOT NULL
 ORDER BY 1,2
 
 --FILTERED USING WHERE CLAUSE "iso_code NOT like 'OWID%'" TO REMOVE UNNECESSARY RECORDS FROM THE DATASET.
---OUR WORLD IN DATA(OWID RECORDS WITH EITHER NO SPECIFIC COUNTRY NAME/LOCATION OR CONTINENT)
+--OUR WORLD IN DATA(OWID RECORDS WITH EITHER NO SPECIFIC COUNTRY NAME/LOCATION OR CONTINENT OR with iso_code starts with OWID)
 
 --Total cases vs population
 --Shows what percentage of population is infected with covid
 
-SELECT location, date, population,total_cases, round((total_cases/population)*100,2) AS 'Infection Percentage'
-FROM PortfolioProject..CovidDeaths
-WHERE population IS NOT NULL
-AND iso_code NOT like 'OWID%'
-ORDER BY 1,2
-
+ 
 
 --HIGHEST AND LOWEST INFECTION RATES
 
@@ -149,7 +144,9 @@ where continent is null
 group by location
 order by TotalDeathCount desc
 
- --CORRECT QUERY USING SUM OF MAX(TOTAL DEATHS BY CONTINENT)
+
+--SKILL: USING SUBQUERIES
+ --CORRECT QUERY USING SUM OF MAX(HIGHEST TOTAL DEATHS BY CONTINENT)
  select continent, sum(maxtotals) as totalDeathsPerContinent from
 (
 select continent, location, MAX(CAST(total_deaths as int)) AS maxtotals
@@ -159,4 +156,135 @@ group by continent, location
 ) a
 group by continent
 order by totalDeathsPerContinent DESC
+
+--test
+ select location, maxtotals from
+(
+select location, MAX(CAST(total_deaths as int)) AS maxtotals
+FROM PortfolioProject..CovidDeaths
+WHERE iso_code NOT like 'OWID%'
+AND continent='Asia'
+group by location
+) a
+order by maxtotals desc
+
+
+--GLOBAL NUMBERS
+--total cases, total deaths and death percentage(deaths/cases) per date
+SELECT date,  sum(new_cases) as total_cases, sum(cast(new_deaths as int)) as total_deaths,  round((sum(cast(new_deaths as int))/sum(new_cases))*100,2) AS 'Death Percentage'
+FROM PortfolioProject..CovidDeaths
+WHERE population IS NOT NULL
+AND iso_code NOT like 'OWID%'
+group by date
+ORDER BY 1,2
+
+--total cases as of november 22, 2022
+select sum(new_cases) as overall_cases, sum(cast(new_deaths as int)) as overall_deaths, round((sum(cast(new_deaths as int))/sum(new_cases))*100,2) AS 'Death Percentage'
+from PortfolioProject..CovidDeaths
+WHERE population IS NOT NULL
+AND iso_code NOT like 'OWID%'
+
+--death percentage deaths/population
+select sum(population) as total_population, sum(new_cases) as overall_cases, sum(cast(new_deaths as int)) as overall_deaths, round((sum(cast(new_deaths as int))/sum(population)),5) AS 'Death Percentage'
+from PortfolioProject..CovidDeaths
+WHERE population IS NOT NULL
+AND iso_code NOT like 'OWID%'
+
+
+
+
+--VACCINATIONS
+
+
+
+--total population vs vaccinations
+
+SELECT * from PortfolioProject..CovidVaccinations
+
+--SKILL: USING OVER PARTITION
+
+--SHOWS ERROR: ORDER BY list of RANGE window frame has total size of 1020 bytes. Largest size supported is 900 bytes.
+--ADDED: ROWS UNBOUNDED PRECEEDING
+--FROM STACKOVERFLOW.COM ROWS UNBOUNDED PRECEDING
+--Together with the ORDER BY it defines the window on which the result is calculated.
+
+--Running total of vaccinated
+select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations,
+sum(convert(bigint, vac.new_vaccinations)) OVER (Partition by dea.location order by dea.location, dea.date ROWS UNBOUNDED PRECEDING) AS vaccinated_running_total
+from PortfolioProject..CovidDeaths dea
+join PortfolioProject..CovidVaccinations vac
+on dea.location=vac.location
+and dea.date=vac.date
+WHERE dea.population IS NOT NULL
+AND dea.iso_code NOT like 'OWID%'
+order by 2,3
+
+--SKILL: USING CTE
+
+--people vaccination versus population percentage
+
+--with must have same columns with subquery
+
+--ERROR: The ORDER BY clause is invalid in views, inline functions, derived tables, subqueries, and common table expressions, unless TOP, OFFSET or FOR XML is also specified.
+--SOLUTION: REMOVED ORDER BY 2,3 IN THE INNER QUERY SINCE IT IS ALREADY USED IN THE PARTITION CLAUSE
+
+With PopuVsVac (continent, location, date, population, new_vaccinations, vaccinated_running_total)
+as 
+(
+
+select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations,
+sum(convert(bigint, vac.new_vaccinations)) OVER (Partition by dea.location order by dea.location, dea.date ROWS UNBOUNDED PRECEDING) AS vaccinated_running_total
+from PortfolioProject..CovidDeaths dea
+join PortfolioProject..CovidVaccinations vac
+on dea.location=vac.location
+and dea.date=vac.date
+WHERE dea.population IS NOT NULL
+AND dea.iso_code NOT like 'OWID%'
+--AND dea.location like '%Philippines%'
+)
+Select *, round((vaccinated_running_total/population)*100,2) as vaccination_percentage from PopuVsVac
+
+
+
+--SKILL: CREATING TEMP TABLES
+DROP Table if exists #PercentPopulationVaccinated
+Create Table #PercentPopulationVaccinated
+(
+Continent nvarchar(255),
+Location nvarchar(255),
+date datetime,
+population numeric,
+new_vaccinations numeric,
+vaccinated_running_total numeric
+)
+
+insert into #PercentPopulationVaccinated
+select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations,
+sum(convert(bigint, vac.new_vaccinations)) OVER (Partition by dea.location order by dea.location, dea.date ROWS UNBOUNDED PRECEDING) AS vaccinated_running_total
+from PortfolioProject..CovidDeaths dea
+join PortfolioProject..CovidVaccinations vac
+on dea.location=vac.location
+and dea.date=vac.date
+WHERE dea.population IS NOT NULL
+AND dea.iso_code NOT like 'OWID%'
+--AND dea.location like '%Philippines%'
+
+Select *, round((vaccinated_running_total/population)*100,2) as vaccination_percentage 
+from #PercentPopulationVaccinated
+
+
+--SKILL: Creating views for data visualizations
+
+Create View PercentPopulationVaccinated as
+select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations,
+sum(convert(bigint, vac.new_vaccinations)) OVER (Partition by dea.location order by dea.location, dea.date ROWS UNBOUNDED PRECEDING) AS vaccinated_running_total
+from PortfolioProject..CovidDeaths dea
+join PortfolioProject..CovidVaccinations vac
+on dea.location=vac.location
+and dea.date=vac.date
+WHERE dea.population IS NOT NULL
+AND dea.iso_code NOT like 'OWID%'
+
+select * from PercentPopulationVaccinated
+
 
